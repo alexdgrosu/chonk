ae73522bed0bcb376e53874c454cfda2b1280670
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2021-04-04 19:33:01 +0200

    🍺 Add simple app exception page

--- src/chonk.self/src/Chonk.Web/Startup.cs
+++ src/chonk.self/src/Chonk.Web/Startup.cs
@@ -1,7 +1,10 @@
+using System.Net;
 using Chonk.Services;
 using Chonk.Services.Settings;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -34,6 +37,13 @@ namespace Chonk.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // * Simple HTML page that offers a truncated version of an exception message.
+                // Is it a terrible idea? Yes.
+                // Does the CHONK like to live dangeriously? Also yes.
+                app.UseAppExceptionPage();
+            }
 
             app.UseRouting();
 
@@ -55,6 +65,34 @@ namespace Chonk.Web
         {
             services.Configure<WorkloadsSettings>(config.GetSection("Workloads"));
             services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<WorkloadsSettings>>().Value);
+        }
+    }
+
+    public static class IApplicationBuilderExtensions
+    {
+        public static void UseAppExceptionPage(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "text/html";
+
+                    var feature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (feature is not null)
+                    {
+                        await context.Response.WriteAsync($@"
+<!DOCTYPE html>
+<title>Internal Server Error</title>
+<h1>🦔: Oops, something went wrong!</h1>
+<p> Here's a sneak peek: <pre><code>{truncate(feature.Error.Message)}...</code></pre></p>");
+                    }
+
+                    static string truncate(string original, int length = 50) => original?.Length < length ? original : original.Substring(0, length);
+                });
+            });
         }
     }
 }
